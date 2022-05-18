9fe6fc13b54d79979a291543bdbf7e8e5c69da90
Villegas Ramirez Luis Eduardo <60408295+EduardoVillegas17@users.noreply.github.com>
2022-05-17 20:18:32 -0700

    Assignamment2

--- practice.js
+++ practice.js
@@ -1,4 +1,4 @@
-let userName=prompt("Enter your name");
+/* let userName=prompt("Enter your name");
 let email=prompt("Enter your email");
 let password=prompt("Enter your password");
 let age=prompt("Enter your age");
@@ -6,19 +6,35 @@ let country=prompt("Enter your country");
 let salaryMonthy=prompt("Enter you monthy salary");
     let salaryYearly=salaryMonthy*12;
 
-  /*   document.write(`
-    <div class="container">
-        <p><span>name: </span> ${userName}</p>
-        <p><span>Email: </span> ${email}</p>
-        <p><span>Password: </span> ${password}</p>
-        <p><span>Age: </span> ${age}</p>
-        <p><span>Country </span> ${country}</p>
-        <p><span>Salary yearly: </span> ${salaryYearly}</p>
-    </div>
-    `) */
+//   document.write(`
+//    <div class="container">
+//        <p><span>name: </span> ${userName}</p>
+//        <p><span>Email: </span> ${email}</p>
+//        <p><span>Password: </span> ${password}</p>
+//        <p><span>Age: </span> ${age}</p>
+//        <p><span>Country </span> ${country}</p>
+//        <p><span>Salary yearly: </span> ${salaryYearly}</p>
+//    </div>
+//    `)
 console.log({userName});
 console.log({email});
 console.log({password});
 console.log({age});
 console.log({country});
-console.log({salaryYearly});
+console.log({salaryYearly}); */
+let productName=prompt("Please enter Product name");
+let priceUnitary=prompt("Please enter Price of the product");
+let quantity=prompt("Please enter Quantity");
+    let subTotal=priceUnitary*quantity;
+    let iva=subTotal*.08;
+let total=subTotal+iva;
+
+document.write(`<div class="container">
+    <p><span>Product Name: </span>${productName}</p>
+    <p><span>Price Unitary: </span>${priceUnitary}</p>
+    <p><span>Quantity: </span>${quantity}</p>
+    <p><span>Sub Total: </span>${subTotal}</p>
+    <p><span>IVA (8%): </span>${iva}</p>
+    <p><span>TOTAL: </span>${total}</p>
+
+</div>`)
