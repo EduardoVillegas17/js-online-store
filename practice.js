02d74557087e57c644622091f9c8bd5aea98b051
Villegas Ramirez Luis Eduardo <60408295+EduardoVillegas17@users.noreply.github.com>
2022-05-17 19:20:59 -0700

    getting values

---
+++ practice.js
@@ -0,0 +1,24 @@
+let userName=prompt("Enter your name");
+let email=prompt("Enter your email");
+let password=prompt("Enter your password");
+let age=prompt("Enter your age");
+let country=prompt("Enter your country");
+let salaryMonthy=prompt("Enter you monthy salary");
+    let salaryYearly=salaryMonthy*12;
+
+  /*   document.write(`
+    <div class="container">
+        <p><span>name: </span> ${userName}</p>
+        <p><span>Email: </span> ${email}</p>
+        <p><span>Password: </span> ${password}</p>
+        <p><span>Age: </span> ${age}</p>
+        <p><span>Country </span> ${country}</p>
+        <p><span>Salary yearly: </span> ${salaryYearly}</p>
+    </div>
+    `) */
+console.log({userName});
+console.log({email});
+console.log({password});
+console.log({age});
+console.log({country});
+console.log({salaryYearly});
---
+++ script.js
@@ -0,0 +1,28 @@
+console.log("Script");
+
+let userDB="admin@admin.com"
+
+//------------------------------------
+
+/* let userName=prompt("Enter you name");
+const URL="http://myserver.com/myendpoint";
+if(userName==userDB){
+    document.write(`
+    <h3>Welcome ${userName.toUpperCase()}</h3>
+    <p>Your name has ${userName.length} letters. </p>
+    `);
+} else{
+    document.write(
+        `<p class="error">Invalid credentials</p>`
+    );
+} */
+/* loops
+console.log(1);
+console.log(2);
+console.log(3);
+console.log(4);
+console.log(5); */
+for(let i=1;i<=100;i++){
+    let multi=i*2;
+    console.log(`${i}*2=${multi}`);
+}
